adf5d9487012901f061f0e2a151ed7cebbafd833
Manu <manuelass.dev@gmail.com>
2025-10-06 12:19:41 -0300

    chore: removendo css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import InputField from './components/InputField';
 import MinhaRua from './assets/img/truck.gif'
 import Footer from './components/Footer';
